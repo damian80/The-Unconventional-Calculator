f41b583131755475a6d7b2c61773ad4f211a32a0
Damian Ciasnocha <daneek80@gmail.com>
2020-02-19 06:18:14 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('that is working');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+addBtn.addEventListener("click", add);
+
+outputResult(currentResult, calculationDescription);
